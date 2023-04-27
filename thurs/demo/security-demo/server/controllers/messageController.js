5ac82798ab2d922364a54458c8afaa8dd33256b0
Mitchell Nguyen <mitch01nguyen@gmail.com>
2023-04-27 12:51:33 -0500

    adds solution to demo

--- thurs/demo/security-demo/server/controllers/messageController.js
+++ thurs/demo/security-demo/server/controllers/messageController.js
@@ -1,31 +1,48 @@
+//import bcrypt
 const bcrypt = require('bcryptjs')
-const chats = []
+let chats = []
+
+  //model of chats
+  //append if pin exists
+  // {
+  //   message: ["hakuna matana",'hakuyta'],
+  //   pin: 1234
+  // },
+  // {
+  //   message: ['hello'],
+  //   pin: 5678
+  // }
 
 module.exports = {
-    createMessage: (req, res) => {
-      console.log(req.body)
-        const { pin, message } = req.body
-        for (let i = 0; i < chats.length; i++) {
-          const existing = bcrypt.compareSync(pin, chats[i].pinHash)
-          if (existing) {
-            chats[i].messages.push(message)
-            let messagesToReturn = {...chats[i]}
-            delete messagesToReturn.pinHash
-            res.status(200).send(messagesToReturn)
-            return
-          }
-        }
+  createMessage : (req,res) => {
+    //deconstruct the body
+    const {pin,message} = req.body
+    //look if pin already exist
+    //if true, append
+    //else create new obj
+    for(let i =0; i < chats.length;i++){
+      //compare a string with hash 
+      const existing = bcrypt.compareSync(pin,chats[i].pinHash)
+      if(existing){
+        chats[i].message.push(message)
+        let messageToReturn = {...chats[i]}
+        delete messageToReturn.pinHash
+        res.status(200).send(messageToReturn)
+        return
+      }
 
-        const salt = bcrypt.genSaltSync(5)
-        const pinHash = bcrypt.hashSync(pin, salt)
-
-        let msgObj = {
-          pinHash,
-          messages: [message]
-        }
-        chats.push(msgObj)
-        let messagesToReturn = {...msgObj}
-        delete messagesToReturn.pinHash
-        res.status(200).send(messagesToReturn)
     }
+    //encrypt pin before sending
+    const salt = bcrypt.genSaltSync(10)
+    const pinHash = bcrypt.hashSync(pin,salt)
+    const newObj = {
+      pinHash,
+      message: [message]
+    }
+    chats.push(newObj)
+    let messageToReturn = {...newObj}
+    delete messageToReturn.pinHash
+    console.log(chats)
+    res.status(200).send(newObj)
+  }
 }
